904cbc06224349f00b87c82ecbe3e9d8b1da4af0
snpsuen <66404197+snpsuen@users.noreply.github.com>
2022-09-19 12:15:51 +0800

    Update app.py

--- app.py
+++ app.py
@@ -15,15 +15,12 @@ def mlp_iris_home():
     return render_template('mlp_iris_index.html')
 
 @app.route('/mlp_iris_prediction', methods=['GET', 'POST'])
-def prediction():
+def mlp_iris_prediction():
   if request.method == 'POST':
-    n_steps = 5
-    past=[]
-    for i in range(1, n_steps + 1):
-      month = "month0" + str(i)
-      result = float(request.form.get(month))
-      past.append(result)
+    characteristics=[]
+    for feature in ["sepallength", "sepalwidth", "petallength", "petalwidth"]
+      result = float(request.form.get(feature))
+      characteristics.append(result)
 
-    row = asarray(past).reshape((1, n_steps, 1))
     yhat = model.predict(row)
     return render_template('result.html', result=yhat)
